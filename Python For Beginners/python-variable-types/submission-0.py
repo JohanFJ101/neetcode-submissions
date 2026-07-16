6de659d1e08c72f3a9f03c44b791a35ad709f7b0
Johan Jagalur <johan.jagalurf@gmail.com>
2026-07-16 01:42:17 -0700

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,13 @@
+integer_type = 12
+float_type = 12.1
+bool_type = True
+string_type = "t"
+list_type = [1,2,3]
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
